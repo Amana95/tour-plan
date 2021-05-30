a88bde58bd85944ff20862ad985b089270e373f8
Amana95 <muradova.aminat@mail.ru>
2021-05-30 11:59:42 +0300

    Добавлено модальное окно

--- js/main.js
+++ js/main.js
@@ -1,27 +1,49 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function(){
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+  });
+  const reviewsSlider = new Swiper('.reviews-container', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function()
+  {
+    document
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
-  },
-});
-const reviewsSlider = new Swiper('.reviews-container', {
-  // Optional parameters
-  loop: true,
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal)
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
+    function openModal(){
+      var modalOverlay=$(".modal__overlay");
+      var modalDialog=$(".modal__dialog");
+      modalOverlay.addClass('modal__overlay--visible');
+      modalDialog.addClass('modal__dialog--visible');
+    }
+    function closeModal(event){
+      event.preventDefault();
+      var modalOverlay=$(".modal__overlay");
+      var modalDialog=$(".modal__dialog");
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function()
-{
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
-})
